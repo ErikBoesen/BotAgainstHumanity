ae31f083750e14771adb0992e81314c327c67aa5
ErikBoesen <me@erikboesen.com>
2019-05-24 13:26:14 -0400

    Start writing Game docstrings

--- bot.py
+++ bot.py
@@ -41,6 +41,11 @@ class Player:
 
 class Game:
     def __init__(self, group_id):
+        """
+        Start game.
+
+        :param group_id: ID of group in which to create game.
+        """
         self.group_id = group_id
         self.players = {}
         self.selection = []
@@ -50,24 +55,33 @@ class Game:
         self.choose_black_card()
 
     def build_decks(self):
+        """
+        Generate black and white decks.
+        """
         self.build_black_deck()
         self.build_white_deck()
 
     def build_black_deck(self):
+        """
+        Read black cards from file.
+        """
         with open("resources/cah/black.json", "r") as f:
-            self.black = json.load(f)
+            self.black_deck = json.load(f)
         # Filter out Pick 2 cards for now
-        self.black = [card for card in self.black if card.count("_") == 1]
-        self.black = [card.replace("_", "_" * 5) for card in self.black]
-        random.shuffle(self.black)
+        self.black_deck = [card for card in self.black_deck if card.count("_") == 1]
+        self.black_deck = [card.replace("_", "_" * 5) for card in self.black_deck]
+        random.shuffle(self.black_deck)
 
     def build_white_deck(self):
+        """
+        Read white cards from file.
+        """
         with open("resources/cah/white.json", "r") as f:
-            self.white = json.load(f)
-        random.shuffle(self.white)
+            self.white_deck = json.load(f)
+        random.shuffle(self.white_deck)
 
     def choose_black_card(self):
-        self.current_black_card = self.black.pop()
+        self.current_black_card = self.black_deck.pop()
 
     def assign_czar(self, user_id=None):
         if user_id is None:
@@ -86,7 +100,7 @@ class Game:
 
     def deal(self, user_id):
         for i in range(self.hand_size):
-            self.players[user_id].pick_up_white(self.white.pop())
+            self.players[user_id].pick_up_white(self.white_deck.pop())
 
     def has_played(self, user_id):
         """
@@ -103,7 +117,7 @@ class Game:
         card = self.players[user_id].hand.pop(card_index)
         self.selection.append((user_id, card))
         # TODO: this is repeated from above, make a method to draw cards
-        self.players[user_id].pick_up_white(self.white.pop())
+        self.players[user_id].pick_up_white(self.white_deck.pop())
         return True
 
     def players_needed(self):
@@ -134,66 +148,19 @@ class Game:
     def discard(self, user_id):
         if user_id not in self.players:
             return False
-        self.white = self.players[user_id].discard_all() + self.white
+        self.white_deck = self.players[user_id].discard_all() + self.white_deck
         self.deal(user_id)
     """
 
 
-class CardsAgainstHumanity(Module):
-    DESCRIPTION = "Play everyone's favorite card game for terrible people. Commands: start, end"
-    ARGC = 1
+games = {}
+# TODO: use references to Player objects??
+playing = {}
 
-    def __init__(self):
-        super().__init__()
-        self.games = {}
-        # TODO: use references to Player objects??
-        self.playing = {}
-
-    def add_player(self, group_id, user_id, name):
-        # This is a function so that it can be called automatically when a user is joining or when they're starting a game
-        self.playing[user_id] = group_id
-        self.games[group_id].join(user_id, name)
-
-    def response(self, query, message):
-        # TODO: fix this mess
-        arguments = query.split()
-        command = arguments.pop(0)
-        group_id = message.group_id
-        user_id = message.user_id
-        name = message.name
-        if command == "start":
-            if group_id in self.games:
-                return "Game already started!"
-            self.games[group_id] = Game(group_id)
-            self.add_player(group_id, user_id, name)
-            return (f"Cards Against Humanity game started. {name} added to game as first Czar. Play at https://yalebot.herokuapp.com/cah/join.\n"
-                    "Other players can say !cah join to join. !cah end will terminate the game.\n")
-        elif command == "end":
-            if group_id not in self.games:
-                return "No game in progress."
-            game = self.games.pop(group_id)
-            for user_id in game.players:
-                self.playing.pop(user_id)
-            return "Game ended. Say !cah start to start a new game."
-        elif command == "join":
-            if user_id in self.playing:
-                return "You're already in a game."
-            if group_id not in self.games:
-                return "No game in progress. Say !cah start to start a game."
-            self.add_player(group_id, user_id, name)
-            return f"{name} has joined the game! Please go to https://yalebot.herokuapp.com/cah/join to play."
-        elif command == "leave":
-            if user_id in self.playing:
-                self.playing.pop(user_id)
-                return f"Removed {name} from the game."
-            else:
-                return f"{name} is not currently in a game."
-        elif command == "info":
-            return str(self.games) + " " + str(self.playing) + " " + str(self)
-        """
-        elif command == "refresh":
-            self.games[group_id].refresh(user_id)
-        """
+def add_player(self, group_id, user_id, name):
+    # This is a function so that it can be called automatically when a user is joining or when they're starting a game
+    self.playing[user_id] = group_id
+    self.games[group_id].join(user_id, name)
 
     def get_user_game(self, user_id):
         game_group_id = self.playing.get(user_id)
@@ -209,6 +176,42 @@ def process_message(message):
             instructions = message.text[len(PREFIX):].strip().split(None, 1)
             command = instructions.pop(0).lower()
             query = instructions[0] if len(instructions) > 0 else ""
+            group_id = message["group_id"]
+            user_id = message["user_id"]
+            name = message["name"]
+            if command == "start":
+                if group_id in self.games:
+                    return "Game already started!"
+                self.games[group_id] = Game(group_id)
+                self.add_player(group_id, user_id, name)
+                return (f"Cards Against Humanity game started. {name} added to game as first Czar. Play at https://yalebot.herokuapp.com/cah/join.\n"
+                        "Other players can say !cah join to join. !cah end will terminate the game.\n")
+            elif command == "end":
+                if group_id not in self.games:
+                    return "No game in progress."
+                game = self.games.pop(group_id)
+                for user_id in game.players:
+                    self.playing.pop(user_id)
+                return "Game ended. Say !cah start to start a new game."
+            elif command == "join":
+                if user_id in self.playing:
+                    return "You're already in a game."
+                if group_id not in self.games:
+                    return "No game in progress. Say !cah start to start a game."
+                self.add_player(group_id, user_id, name)
+                return f"{name} has joined the game! Please go to https://yalebot.herokuapp.com/cah/join to play."
+            elif command == "leave":
+                if user_id in self.playing:
+                    self.playing.pop(user_id)
+                    return f"Removed {name} from the game."
+                else:
+                    return f"{name} is not currently in a game."
+            elif command == "info":
+                return str(self.games) + " " + str(self.playing) + " " + str(self)
+            """
+            elif command == "refresh":
+                self.games[group_id].refresh(user_id)
+            """
             """
             if command == "help":
                 if query:
@@ -287,7 +290,7 @@ def manager():
             "name": "Bot Against Humanity",
             "group_id": group_id,
             "avatar_url": "https://i.groupme.com/200x200.png.092e3648ee2745aeb3296a51b3a85e0f",
-            "callback_url": "https://botagainsthumanitygroupme.herokuapp.com/",
+            "callback_url": "https://botagainsthumanitygroupme.herokuapp.com/message",
         }
         me = requests.get(f"https://api.groupme.com/v3/users/me?token={access_token}").json()["response"]
         result = requests.post(f"https://api.groupme.com/v3/bots?token={access_token}",
